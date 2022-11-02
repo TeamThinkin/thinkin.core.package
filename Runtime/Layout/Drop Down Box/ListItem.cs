18df2f9d1e0be67fff451d6a93c911d443e8bbfd
greengiant83 <greengiant83@hotmail.com>
2022-11-02 14:48:18 -0500

    refactoring tablet panels

---
+++ Runtime/Layout/Drop Down Box/ListItem.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListItemDto
+{
+    public object Value { get; set; }
+    public string Text { get; set; }
+}
+
+public class ListItem : ButtonInteractable
+{
+    public DropDownBox ParentListControl;
+    public ListItemDto Dto { get; private set; }
+
+    public void SetDto(ListItemDto Dto)
+    {
+        this.Dto = Dto;
+        Label.text = Dto.Text;
+    }
+
+    protected override void Pressed()
+    {
+        base.Pressed();
+        ParentListControl.ListItem_Selected(this);
+    }
+}
