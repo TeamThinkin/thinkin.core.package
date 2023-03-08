66efa541191483b13fe887a821118808bca299a2
greengiant83 <greengiant83@hotmail.com>
2023-03-08 15:20:47 -0600

    switched to a single tablet system

--- Runtime/System/ItemSpawnObserver.cs
+++ Runtime/System/ItemSpawnObserver.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public static class ItemSpawnObserver
 {
     public static event System.Action<GameObject> OnItemSpawned;
+    public static event System.Action<GameObject> OnItemDespawned;
 
     public static void NotifyItemSpawned(GameObject SpawnedItem)
     {
         OnItemSpawned?.Invoke(SpawnedItem);
+    }
+
+    public static void NotifyItemDespawned(GameObject DespawnedItem)
+    {
+        OnItemDespawned?.Invoke(DespawnedItem);
     }
 }
 
